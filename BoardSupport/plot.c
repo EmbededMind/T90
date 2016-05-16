bbd66f132208cd4d6b9eacc402a169a12e6b694e
zhangx2016 <zhangx2016@outlook.com>
2016-05-16 11:18:53 +0800

    啊

--- BoardSupport/plot.c
+++ BoardSupport/plot.c
@@ -66,7 +66,7 @@ void DrawShipFamily(int flag)
 		GUI_DrawVLine(40, motherShipPixel.y, t90_set.dst.dst3*M_TO_MILLINM*TO_PIXEL/2 - 20 + motherShipPixel.y);
 		GUI_DrawVLine(40, t90_set.dst.dst3*M_TO_MILLINM*TO_PIXEL/2 + 20 + motherShipPixel.y, t90_set.dst.dst3*M_TO_MILLINM*TO_PIXEL + motherShipPixel.y);
 		sprintf(pStrBuf,"%dm",t90_set.dst.dst3);	
-		GUI_DispStringAt(pStrBuf, 40 - GUI_GetStringDistX(pStrBuf)/2 + 3, t90_set.dst.dst3*M_TO_MILLINM*TO_PIXEL/2 - 20 + motherShipPixel.y);
+		GUI_DispStringAt(pStrBuf, 40 - GUI_GetStringDistX(pStrBuf)/2 + 3, t90_set.dst.dst3*M_TO_MILLINM*TO_PIXEL/2 - 12 + motherShipPixel.y);
 	}
 	
 	GUI_SetLineStyle(GUI_LS_DOT);
