6c50e00bed40305ecbc6cdcc16f2bee802c9081d
zhangx2016 <zhangx2016@outlook.com>
2016-05-16 10:33:54 +0800

    add

---
+++ BoardSupport/transform.h
@@ -0,0 +1,40 @@
+#ifndef _TRANSFORM_H
+#define _TRANSFORM_H
+
+
+#include "boat_struct.h"
+
+
+void llToxy(BERTH *pBerth);
+
+int getAngleOfShip(BERTH *pBerth);
+
+int SND_ParseNum(int num, uint8_t *pNum);
+
+
+
+
+
+
+
+
+
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
