bdce894e37587e7970bb257acb7f2e51fb818d3b
SealedGhsot <815407069@qq.com>
2016-07-09 12:24:33 +0800

    Feature about    Modify| 更改了T90与T81的通信方式

--- BoardSupport/comm.h
+++ BoardSupport/comm.h
@@ -1,6 +1,11 @@
 #ifndef _COMM_H
 #define _COMM_H
+#include <lpc_types.h>
 
-
+void Comm_sendPulse(void);
+void Comm_addFrame( uint8_t whichPort, uint16_t offset_x, uint16_t offset_y );
+uint8_t* Comm_fetchNextFrame(void);
+void Comm_sendFrame(uint8_t* pFrame);
+//void Comm_sendFrame(uint8_t whichPort, uint16_t offset_x, uint16_t offset_y);
 
 #endif
